59f794f585a2f52c2bc0d7447a52ef5ec8ab4437
AMOREM <amorem@amorem.com>
2019-07-20 09:06:49 +0200

    Updates

---
+++ precache-manifest.0044e63f941f2089db5b5ffd76eaf484.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "334a9846c7cb3dbdaaecd091a13dfd6b",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "12c94de18f957f7d2342",
+    "url": "/monsters-rolodex/static/css/main.082b6e0a.chunk.css"
+  },
+  {
+    "revision": "458a39bd49d85bb2560b",
+    "url": "/monsters-rolodex/static/js/2.b0d4d72d.chunk.js"
+  },
+  {
+    "revision": "12c94de18f957f7d2342",
+    "url": "/monsters-rolodex/static/js/main.0e0c1552.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
